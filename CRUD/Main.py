72f21b96f662c90b33eef1a1f2c2671e90cf8ca5
Drewkilleen317 <93172549+Drewkilleen317@users.noreply.github.com>
2022-12-11 23:59:06 -0500

    12/11

--- CRUD/Main.py
+++ CRUD/Main.py
@@ -3,63 +3,63 @@ import sqlite3
 import streamlit as st
 import pandas as pd
 from bisect import bisect
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, ColumnsAutoSizeMode
 
 db_file = 'Data/Daily_Measures.db'
 
 conn = sqlite3.connect(db_file)
-
 cur = conn.cursor()
 
 TODs = ("Morning", "Midday", "Night")
 measures = ('Glucose', 'Keytones', 'Weight', 'BP-S', 'BP-D', 'Uric Acid')
 
-
+#==========================================================================================
 def Create(DB):
     st.subheader("Create Records")
 
-    select_query = """SELECT * FROM MEASUREVALUES ORDER BY ROWID DESC LIMIT 5;"""
+    # select_query = """SELECT * FROM MEASUREVALUES ORDER BY ROWID DESC LIMIT 5;"""
 
-    create_query = """INSERT INTO MEASUREVALUES
-                            (Date,
-                            TOD,
-                            Measure,
-                            Value)
-                            VALUES (?, ?, ?, ?);"""
+    # create_query = """INSERT INTO MEASUREVALUES
+    #                         (Date,
+    #                         TOD,
+    #                         Measure,
+    #                         Value)
+    #                         VALUES (?, ?, ?, ?);"""
 
-    currentDateTime = datetime.now()
-    tods = ("Night","Morning","Midday","Night",)
-    tod_breaks = (0, 6, 12, 20, 24)
-    tod_index = bisect(tod_breaks,currentDateTime.hour)-1
-    tod = tods[tod_index]
+    # currentDateTime = datetime.now()
+    # tods = ("Night","Morning","Midday","Night",)
+    # tod_breaks = (0, 6, 12, 20, 24)
+    # tod_index = bisect(tod_breaks,currentDateTime.hour)-1
+    # tod = tods[tod_index]
 
-    with st.form(key='query_form'):
-            date = st.date_input(
-                'Enter Date', value=currentDateTime.date())
-            TOD = st.selectbox("Select Time of Day", TODs, index=TODs.index(tod) )
-            measure = st.selectbox("Select Data Point", measures, index=0 )
+    # with st.form(key='query_form'):
+    #         date = st.date_input(
+    #             'Enter Date', value=currentDateTime.date())
+    #         TOD = st.selectbox("Select Time of Day", TODs, index=TODs.index(tod) )
+    #         measure = st.selectbox("Select Data Point", measures, index=0 )
 
-            value = st.number_input('Enter Data Value', step=1e-1, format="%.1f")
+    #         value = st.number_input('Enter Data Value', step=1e-1, format="%.1f")
 
-            data_tuple = (date,
-                          TOD,
-                          measure,
-                          value)
+    #         data_tuple = (date,
+    #                       TOD,
+    #                       measure,
+    #                       value)
 
-            submit_data = st.form_submit_button("Submit Data")
+    #         submit_data = st.form_submit_button("Submit Data")
 
-    if submit_data:
+    # if submit_data:
                 
 
-                conn = sqlite3.connect(db_file)
-                cur = conn.cursor()
-                try:
-                    cur.execute(create_query, data_tuple)
-                    st.info("Data Submitted Successfully")
-                    conn.commit()
-                except sqlite3.Error as er:
-                    st.info("Error Submitting Record: " + str(er))
+    #             conn = sqlite3.connect(db_file)
+    #             cur = conn.cursor()
+    #             try:
+    #                 cur.execute(create_query, data_tuple)
+    #                 st.info("Data Submitted Successfully")
+    #                 conn.commit()
+    #             except sqlite3.Error as er:
+    #                 st.info("Error Submitting Record: " + str(er))
 
-                df = pd.read_sql(select_query, con=conn)
+    #             df = pd.read_sql(select_query, con=conn)
 
                 # try:
                 #     cur.execute(select_query)
@@ -67,55 +67,100 @@ def Create(DB):
                 # except sqlite3.Error as er:
                 #     st.info("Error Reading Records: " + str(er))
 
-                with st.expander("Pretty Table"):
-                    st.write(df)
-
-                conn.commit()
-                conn.close()
-def Read(DB):
+                # with st.expander("Pretty Table"):
+                #     st.write(df)
+               
+                # conn.commit()
+                # conn.close()
+#==========================================================================================                
+def Read(page):
     st.subheader("Read Records")
 
+    conn = sqlite3.connect(db_file)
+    cur = conn.cursor()
+    select_query = """SELECT * FROM MEASUREVALUES ORDER BY ROWID DESC;"""
+    data = pd.read_sql(select_query, con=conn)
+    
+    gb = GridOptionsBuilder.from_dataframe(data)
+    gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+    gb.configure_side_bar() #Add a sidebar
+    gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") 
+    gb.configure_column("Date", header_name="Date", editable=True)
+    gb.configure_column("TOD", header_name="TOD", editable=True)
+    gridOptions = gb.build()
+
+    grid_response = AgGrid(
+        data,
+        gridOptions=gridOptions,
+        data_return_mode='AS_INPUT', 
+        update_mode='MODEL_CHANGED', 
+        fit_columns_on_grid_load=False,
+        enable_enterprise_modules=True,
+        height=350, 
+        width='100%',
+        theme = "streamlit",
+        editable=True,
+        columns_auto_size_mode=ColumnsAutoSizeMode.FIT_CONTENTS
+    )
+    data = grid_response['data']
+    selected = grid_response['selected_rows'] 
+    new_frame = pd.DataFrame(selected)
+    st.write(new_frame)
+    grid_response = AgGrid(
+        new_frame,
+        gridOptions=gridOptions,
+        data_return_mode='AS_INPUT', 
+        update_mode='MODEL_CHANGED', 
+        fit_columns_on_grid_load=False,
+        enable_enterprise_modules=True,
+        height=350, 
+        width='100%',
+        columns_auto_size_mode=ColumnsAutoSizeMode.FIT_CONTENTS
+        )
+    conn.close()
+   
+#==========================================================================================    
 def Update(DB):
     st.subheader("Update Records")
-
+#==========================================================================================
 def Delete(DB):
     st.subheader("Delete Records")
-
+#==========================================================================================
 def analytics(page):
     st.subheader("Analytics")
 
-    query = """SELECT * FROM MEASUREVALUES;"""
-    conn = sqlite3.connect(db_file)
-    df = pd.read_sql(query, con=conn)
+    # query = """SELECT * FROM MEASUREVALUES;"""
+    # conn = sqlite3.connect(db_file)
+    # df = pd.read_sql(query, con=conn)
 
-    measure_options = df['Measure'].unique().tolist()
-    TOD_options = df['TOD'].unique().tolist()
+    # measure_options = df['Measure'].unique().tolist()
+    # TOD_options = df['TOD'].unique().tolist()
 
-    measures_selected = st.multiselect("Select a Measure", measure_options)
-    TODs_selected = st.multiselect("Select TODs", TOD_options)
+    # measures_selected = st.multiselect("Select a Measure", measure_options)
+    # TODs_selected = st.multiselect("Select TODs", TOD_options)
 
-    df = df[df["Measure"].isin(measures_selected)]
-    df = df[df["TOD"].isin(TODs_selected)]
+    # df = df[df["Measure"].isin(measures_selected)]
+    # df = df[df["TOD"].isin(TODs_selected)]
 
-    with st.expander("Pretty Table 1"):
-        st.dataframe(df)
+    # with st.expander("Pretty Table 1"):
+    #     st.dataframe(df)
 
-    conn.close()
-
+    # conn.close()
+#==========================================================================================
 def about(page):
     st.subheader("About")
 
-
+#==========================================================================================
 def data_entry(page):
     st.subheader("Data Entry")
-    insert_query = """INSERT INTO MEASUREVALUES
-                            (Date,
-                            TOD,
-                            Measure,
-                            Value)
-                            VALUES (?, ?, ?, ?);"""
+    # insert_query = """INSERT INTO MEASUREVALUES
+    #                         (Date,
+    #                         TOD,
+    #                         Measure,
+    #                         Value)
+    #                         VALUES (?, ?, ?, ?);"""
 
-    select_query = """SELECT * FROM MEASUREVALUES;"""
+    # select_query = """SELECT * FROM MEASUREVALUES;"""
 
             # if submit_data:
             #     st.info("Data Submitted")
@@ -135,7 +180,7 @@ def data_entry(page):
             #     conn.commit()
             #     conn.close()
 
-
+#==========================================================================================
 def main():
     st.title("Health Database Management")
 
@@ -149,6 +194,6 @@ def main():
     action = actions.get(page)
 
     action('page')
-
+#==========================================================================================
 if __name__ == '__main__':
     main()
